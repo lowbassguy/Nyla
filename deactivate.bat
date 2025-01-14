9704b6949a91e70804112972562d13909cf6b13f
lowbassguy <lowbassguy@gmail.com>
2025-01-14 12:06:21 -0500

    **

---
+++ deactivate.bat
@@ -0,0 +1,4 @@
+@echo off
+set VIRTUAL_ENV=
+set PATH=%PATH:Scripts;=%
+cmd /k "cd %CD% && prompt $P$G" 
--- run_bot.bat
+++ run_bot.bat
@@ -1,4 +1,9 @@
 @echo off
+if not exist "venv" (
+    echo Creating Python 3.12 virtual environment...
+    py -3.12 -m venv venv
+)
+
 echo Activating virtual environment...
 call venv\Scripts\activate
 
